286d19d9aedbea91f3779e6f40c174331c7dfe0e
Jasperpis <ywan0279@student.monash.edu>
2020-05-19 10:08:22 +0800

    commit

--- index.js
+++ index.js
@@ -1,20 +1,111 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+//const bodyParser = require('body-parser');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+//  .use(bodyParser.urlencoded({ extended: false }))
+  //.use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/native.ejs',(req,res) => res.render('pages/native.ejs'))
-  .get('/land.ejs',(req,res) => res.render('pages/land.ejs'))
-  .get('/grant1.ejs',(req,res) => res.render('pages/grant1.ejs'))
-  .get('/grant2.ejs',(req,res) => res.render('pages/grant2.ejs'))
-  .get('/grant3.ejs',(req,res) => res.render('pages/grant3.ejs'))
-  .get('/grant4.ejs',(req,res) => res.render('pages/grant4.ejs'))
-  .get('/grant5.ejs',(req,res) => res.render('pages/grant5.ejs'))
-  .get('/grant6.ejs',(req,res) => res.render('pages/grant6.ejs'))
-  .get('/grant7.ejs',(req,res) => res.render('pages/grant7.ejs'))
-  .get('/grant8.ejs',(req,res) => res.render('pages/grant8.ejs'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      let c1 = req.query.c1;
+        if(c1.length%2 == 1){c1='Shrub';};
+      let c2 = req.query.c2;
+        if(c2.length%2 == 0){c2='Succulent';};
+      let c3 = req.query.c3;
+        if(c3.length%2 == 0){c3='Grass';};
+      let c4 = req.query.c4;
+        if(c4.length%2 == 0){c4='Tree';};
+        
+      let d1 = req.query.d1;
+        if(d1.length%2 == 1){d1='Basal';};
+      let d2 = req.query.d2;
+        if(d2.length%2 == 0){d2='Whorled';};
+      let d3 = req.query.d3;
+        if(d3.length%2 == 0){d3='Alternate';};
+      let d4 = req.query.d4;
+        if(d4.length%2 == 0){d4='Opposite';};
+        
+      let e1 = req.query.e1;
+        if(e1.length%2 == 1){e1='Compound';};
+      let e2 = req.query.e2;
+        if(e2.length%2 == 0){e2='Fern-like';};
+      let e3 = req.query.e3;
+        if(e3.length%2 == 0){e3='Needle';};
+      let e4 = req.query.e4;
+        if(e4.length%2 == 0){e4='Simple';};
+        
+      const result = await client.query("SELECT * FROM weeds Where Growth_Form in ('"+c1+"','"+c2+"','"+c3+"','"+c4+"') or Leaf_Arrangement in ('"+d1+"','"+d2+"','"+d3+"','"+d4+"') or Leaf_Form in ('"+e1+"','"+e2+"','"+e3+"','"+e4+"')");
+      //const result = await client.query("SELECT * FROM test_table Where id='"+num+"'");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/db2', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      let c1 = req.query.c1;
+        if(typeof(c1) == typeof(a)){c1=1;}
+           // else{if(c1 == ){c1='Shrub';}
+             //    if(c1 == '2'){c1='Succulent';}}
+      let c2 = req.query.c2;
+        if(typeof(c2) == typeof(a)){c2=1;}
+           // else{c2='Succulent';}
+      let c3 = req.query.c3;
+        if(typeof(c3) == typeof(a)){c3=1;}
+          // else{c3='Grass';}
+      let c4 = req.query.c4;
+        if(typeof(c4) == typeof(a)){c4=1;}
+          //  else{c4='Tree';}
+        
+      let d1 = req.query.d1;
+        if(typeof(d1) == typeof(a)){d1=1;}
+          //  else{d1="Basal";}
+      let d2 = req.query.d2;
+        if(typeof(d2) == typeof(a)){d2=1;}
+        //    else{d2="Whorled";}
+      let d3 = req.query.d3;
+        if(typeof(d3) == typeof(a)){d3=1;}
+          //  else{d3="Alternate";}
+      let d4 = req.query.d4;
+        if(typeof(d4) == typeof(a)){d4=1;}
+        //    else{d4="Opposite";}
+        
+      let e1 = req.query.e1;
+        if(typeof(e1) == typeof(a)){e1=1;}
+          //  else{e1="Compound";}
+      let e2 = req.query.e2;
+        if(typeof(e2) == typeof(a)){e2=1;}
+        //    else{e2="Fern-like";}
+      let e3 = req.query.e3;
+        if(typeof(e3) == typeof(a)){e3=1;}
+          //  else{e3="Needle";}
+      let e4 = req.query.e4;
+        if(typeof(e4) == typeof(a)){e4=1;}
+        //    else{e4="Simple";}
+      const result = await client.query("SELECT * FROM weeds Where Growth_Form in ('"+c1+"','"+c2+"','"+c3+"','"+c4+"') or Leaf_Arrangement in ('"+d1+"','"+d2+"','"+d3+"','"+d4+"') or Leaf_Form in ('"+e1+"','"+e2+"','"+e3+"','"+e4+"')");
+      //const result = await client.query("SELECT * FROM test_table Where id='"+num+"'");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/js/main.js
+++ public/js/main.js
@@ -1,92 +1,264 @@
-"use strict";
-$(document).ready(function (e) {
+jQuery(document).ready(function($){
+	var	scrolling = false;
+	var contentSections = $('.cd-section'),
+		verticalNavigation = $('.cd-vertical-nav'),
+		navigationItems = verticalNavigation.find('a'),
+		navTrigger = $('.cd-nav-trigger'),
+		scrollArrow = $('.cd-scroll-down');
 
-    new WOW().init();
-    function toggleChevron(e) {
-        $(e.target)
-                .prev('.panel-heading')
-                .find("i.indicator")
-                .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
-    }
-    $('#accordion').on('hidden.bs.collapse', toggleChevron);
-    $('#accordion').on('shown.bs.collapse', toggleChevron);
-    $('.accordion-toggle').click(function () {
+	$(window).on('scroll', checkScroll);
 
-        $('.panel-heading').toggleClass('bg-head');
+	//smooth scroll to the selected section
+	verticalNavigation.on('click', 'a', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+        verticalNavigation.removeClass('open');
     });
 
-    $('#container').mixItUp();
-
-
-//++++++++++++++++++++++++++++++++
-//   All Sliders
-//++++++++++++++++++++++++++++++++
-    $("#owl-demo").owlCarousel({
-        items: 4,
-        lazyLoad: true,
-        navigation: true, itemsDesktop: [1199, 4],
-        itemsDesktopSmall: [980, 3],
-        itemsTablet: [768, 2],
-        itemsTabletSmall: [800, 2],
-        itemsMobile: [479, 1]});
-    $("#owl-demo-2").owlCarousel({
-        items: 4,
-        lazyLoad: true,
-        navigation: true,
-        itemsDesktop: [1199, 4],
-        itemsDesktopSmall: [980, 3],
-        itemsTablet: [768, 2],
-        itemsTabletSmall: [800, 2],
-        itemsMobile: [479, 1]
-    });
-    $("#owl-demo-3").owlCarousel({
-        items: 1,
-        lazyLoad: true,
-        navigation: false,
-        itemsDesktop: [1280, 1],
-        itemsDesktopSmall: [980, 1],
-        itemsTablet: [768, 1],
-        itemsMobile: [479, 1]
-    });
-//    With Arrows
-    $("#main-banner").owlCarousel({
-        items: 1,
-        lazyLoad: true,
-        navigation: true,
-        navigationText: ["prev", "next"],
-        itemsDesktop: [1199, 1],
-        itemsDesktopSmall: [979, 1],
-        itemsTablet: [768, 1]
-    });
-//   Without Arrows
-    $("#main-banner2").owlCarousel({
-        items: 1,
-        lazyLoad: true,
-        navigation: false,
-        navigationText: ["prev", "next"],
-        itemsDesktop: [1199, 1],
-        itemsDesktopSmall: [979, 1],
-        itemsTablet: [768, 1]
+    //smooth scroll to the second section
+    scrollArrow.on('click', function(event){
+    	event.preventDefault();
+        smoothScroll($(this.hash));
     });
 
+	// open navigation if user clicks the .cd-nav-trigger - small devices only
+    navTrigger.on('click', function(event){
+    	event.preventDefault();
+    	verticalNavigation.toggleClass('open');
+    });
 
-//++++++++++++++++++++++++++++++++
-//   Google Analyrics
-//++++++++++++++++++++++++++++++++
-    (function (b, o, i, l, e, r) {
-        b.GoogleAnalyticsObject = l;
-        b[l] || (b[l] =
-                function () {
-                    (b[l].q = b[l].q || []).push(arguments);
-                });
-        b[l].l = +new Date;
-        e = o.createElement(i);
-        r = o.getElementsByTagName(i)[0];
-        e.src = '';
-        r.parentNode.insertBefore(e, r);
-    }
-    (window, document, 'script', 'ga'));
-    ga('create', 'UA-XXXXX-X', 'auto');
-    ga('send', 'pageview');
+	function checkScroll() {
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame) ? setTimeout(updateSections, 300) : window.requestAnimationFrame(updateSections);
+		}
+	}
 
+	function updateSections() {
+		var halfWindowHeight = $(window).height()/2,
+			scrollTop = $(window).scrollTop();
+		contentSections.each(function(){
+			var section = $(this),
+				sectionId = section.attr('id'),
+				navigationItem = navigationItems.filter('[href^="#'+ sectionId +'"]');
+			( (section.offset().top - halfWindowHeight < scrollTop ) && ( section.offset().top + section.height() - halfWindowHeight > scrollTop) )
+				? navigationItem.addClass('active')
+				: navigationItem.removeClass('active');
+		});
+		scrolling = false;
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	300
+        );
+	}
 });
+
+// ------- Masonry End ------- //
+
+
+
+function validate1(val) {
+	v1 = document.getElementById("fname");
+	v2 = document.getElementById("lname");
+	v3 = document.getElementById("email");
+	v4 = document.getElementById("mob");
+	
+	flag1 = true;
+	flag2 = true;
+	flag3 = true;
+	flag4 = true;
+	
+	if(val>=1 || val==0) {
+	if(v1.value == "") {
+	v1.style.borderColor = "red";
+	flag1 = false;
+	}
+	else {
+	v1.style.borderColor = "green";
+	flag1 = true;
+	}
+	}
+	
+	if(val>=2 || val==0) {
+	if(v2.value == "") {
+	v2.style.borderColor = "red";
+	flag2 = false;
+	}
+	else {
+	v2.style.borderColor = "green";
+	flag2 = true;
+	}
+	}
+	
+	if(val>=3 || val==0) {
+	if(v3.value == "") {
+	v3.style.borderColor = "red";
+	flag3 = false;
+	}
+	else {
+	v3.style.borderColor = "green";
+	flag3 = true;
+	}
+	}
+	
+	if(val>=4 || val==0) {
+	if(v4.value == "") {
+	v4.style.borderColor = "red";
+	flag4 = false;
+	}
+	else {
+	v4.style.borderColor = "green";
+	flag4 = true;
+	}
+	}
+	
+	flag = flag1 && flag2 && flag3 && flag4;
+	
+	return flag;
+	}
+	
+	function validate2(val) {
+	v1 = document.getElementById("cname");
+	v2 = document.getElementById("zip");
+	v3 = document.getElementById("state");
+	v4 = document.getElementById("city");
+	
+	flag1 = true;
+	flag2 = true;
+	flag3 = true;
+	flag4 = true;
+	
+	if(val>=1 || val==0) {
+	if(v1.value == "") {
+	v1.style.borderColor = "red";
+	flag1 = false;
+	}
+	else {
+	v1.style.borderColor = "green";
+	flag1 = true;
+	}
+	}
+	
+	if(val>=2 || val==0) {
+	if(v2.value == "") {
+	v2.style.borderColor = "red";
+	flag2 = false;
+	}
+	else {
+	v2.style.borderColor = "green";
+	flag2 = true;
+	}
+	}
+	
+	if(val>=3 || val==0) {
+	if(v3.value == "") {
+	v3.style.borderColor = "red";
+	flag3 = false;
+	}
+	else {
+	v3.style.borderColor = "green";
+	flag3 = true;
+	}
+	}
+	
+	if(val>=4 || val==0) {
+	if(v4.value == "") {
+	v4.style.borderColor = "red";
+	flag4 = false;
+	}
+	else {
+	v4.style.borderColor = "green";
+	flag4 = true;
+	}
+	}
+	
+	flag = flag1 && flag2 && flag3 && flag4;
+	
+	return flag;
+	}
+	
+	$(document).ready(function(){
+	
+	var current_fs, next_fs, previous_fs;
+	
+	$(".next").click(function(){
+	
+	str1 = "next1";
+	str2 = "next2";
+	str3 = "next3";
+	
+	if(!str2.localeCompare($(this).attr('id')) && validate1(0) == true) {
+	val2 = true;
+	}
+	else {
+	val2 = false;
+	}
+	
+	if(!str3.localeCompare($(this).attr('id')) && validate2(0) == true) {
+	val3 = true;
+	}
+	else {
+	val3 = false;
+	}
+	
+	if(!str1.localeCompare($(this).attr('id')) || (!str2.localeCompare($(this).attr('id')) && val2 == true) || (!str3.localeCompare($(this).attr('id')) && val3 == true)) {
+	current_fs = $(this).parent().parent();
+	next_fs = $(this).parent().parent().next();
+	
+	$(current_fs).removeClass("show");
+	$(next_fs).addClass("show");
+	
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	current_fs.animate({}, {
+	step: function() {
+	
+	current_fs.css({
+	'display': 'none',
+	'position': 'relative'
+	});
+	
+	next_fs.css({
+	'display': 'block'
+	});
+	}
+	});
+	}
+	});
+	
+	$(".prev").click(function(){
+	
+	current_fs = $(this).parent().parent();
+	previous_fs = $(this).parent().parent().prev();
+	
+	$(current_fs).removeClass("show");
+	$(previous_fs).addClass("show");
+	
+	$("#progressbar li").eq($("fieldset").index(next_fs)).removeClass("active");
+	
+	current_fs.animate({}, {
+	step: function() {
+	
+	current_fs.css({
+	'display': 'none',
+	'position': 'relative'
+	});
+	
+	previous_fs.css({
+	'display': 'block'
+	});
+	}
+	});
+	});
+	
+	$('.radio-group .radio').click(function(){
+	$(this).toggleClass('selected');
+	});
+	
+	});
+
+
+	/* 8? 8? */
